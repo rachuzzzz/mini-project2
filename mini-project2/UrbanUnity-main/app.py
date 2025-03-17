5b85e89a9ce1be07076da7b3897fbf2e8ab47e58
rachuzzzz <thomasraisen123@gmail.com>
2025-03-17 22:57:44 +0530

    Add files via upload

--- mini-project2/UrbanUnity-main/app.py
+++ mini-project2/UrbanUnity-main/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('landing.html')
 
 @app.route('/citizen-login')
 def citizen_login():
